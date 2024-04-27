28f2f629e4fae930cda623c2e2ae3aadec4e4f0a
mim chowdhury <umac3333@gmail.com>
2024-04-27 20:20:20 +0600

    new-sec-added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import Cancellation from "../Cancellation/Cancellation";
+import WhyUs from "../WhyUs/WhyUs";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <WhyUs></WhyUs>
             <Cancellation></Cancellation>
         </div>
     );
