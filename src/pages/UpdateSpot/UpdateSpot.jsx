f56707319625d0f5acb56a57b627186ca4235654
mim chowdhury <umac3333@gmail.com>
2024-04-30 13:15:42 +0600

    update-added

--- src/pages/UpdateSpot/UpdateSpot.jsx
+++ src/pages/UpdateSpot/UpdateSpot.jsx
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import swal from 'sweetalert';
 import { Flip } from "react-awesome-reveal";
+import { useParams } from 'react-router-dom';
 
 
 
 const UpdateSpot = () => {
+    const { id } = useParams();
+    // console.log(id)
+    const [spots, setSpots] = useState({});
+    const { _id, PhotoURL, AverageCost, SpotName, TotalVisitor, TravelTime,
+        Seasonality,
+        CountryName,
+        Location,
+        Description,
+        Email,
+        UserName
+
+    } = spots;
+    useEffect(() => {
+        fetch(`http://localhost:5000/singleSpot/${id}`)
+            .then(res => res.json())
+            .then(data => setSpots(data)
+            )
+        reset(spots)
+    }, [id]);
+
     const { user } = useContext(AuthContext);
     const {
         register,
-        handleSubmit,
+        handleSubmit, reset
 
-    } = useForm();
-
+    } = useForm()
 
     const onSubmit = data => {
         // console.log(data);
         const newData = data;
         console.log(newData);
+        // update data
+        fetch(`http://localhost:5000/updateSpot/${id}`, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newData)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    swal({
+                        icon: "success",
+                        title: "Tourist Spot Updated successfully",
+                    });
+                }
+            })
+
     }
-    // update data
+
 
 
 
@@ -37,20 +77,21 @@ const UpdateSpot = () => {
                     <div className=" grid gap-3 w-full justify-center mb-3">
                         <div className="form-control ">
                             <label className="input input-bordered flex items-center gap-2">
-                                Photo URL                            <input type="text" name="photo" className="grow w-full" placeholder="Photo URL" {...register("PhotoURL")} />
+                                Photo URL                            <input type="text" required
+                                    defaultValue={PhotoURL} name="photo" className="grow w-full" placeholder="Photo URL" {...register("PhotoURL")} />
                             </label>
                         </div>
                         <div className="grid lg:flex gap-3 w-full">
                             <div className="form-control lg:w-1/2">
                                 <label className="input input-bordered flex items-center gap-2">
                                     Spot Name
-                                    <input type="text" name="spot" className="grow w-full" placeholder="Spot name" {...register("SpotName")} />
+                                    <input type="text" required defaultValue={SpotName} name="spot" className="grow w-full" placeholder="Spot name" {...register("SpotName")} />
                                 </label>
                             </div>
                             <div className="form-control lg:w-1/2">
                                 <label className="input input-bordered flex items-center gap-2">
                                     Country Name
-                                    <input type="text" name="country" className="grow w-full" placeholder="Country Name" {...register("CountryName")} />
+                                    <input type="text" required defaultValue={CountryName} name="country" className="grow w-full" placeholder="Country Name" {...register("CountryName")} />
                                 </label>
                             </div>
                         </div>
@@ -58,13 +99,13 @@ const UpdateSpot = () => {
                             <div className="form-control lg:w-1/2 ">
                                 <label className="input input-bordered flex items-center gap-2">
                                     Location
-                                    <input type="text" name="location" className="grow w-full" placeholder="Location" {...register("Location")} />
+                                    <input type="text" required defaultValue={Location} name="location" className="grow w-full" placeholder="Location" {...register("Location")} />
                                 </label>
                             </div>
                             <div className="form-control lg:w-1/2">
                                 <label className="input input-bordered flex items-center gap-2">
                                     Average Cost
-                                    <input type="text" name="cost" className="grow w-full" placeholder="Average Cost" {...register("AverageCost")} />
+                                    <input type="text" required defaultValue={AverageCost} name="cost" className="grow w-full" placeholder="Average Cost" {...register("AverageCost")} />
                                 </label>
                             </div>
                         </div>
@@ -72,24 +113,24 @@ const UpdateSpot = () => {
                             <div className="form-control lg:w-1/3">
                                 <label className="input input-bordered flex items-center gap-2">
                                     Travel Time
-                                    <input type="text" name="time" className="grow w-full" placeholder="Travel Time in Days" {...register("TravelTime")} />
+                                    <input type="text" required defaultValue={TravelTime} name="time" className="grow w-full" placeholder="Travel Time in Days" {...register("TravelTime")} />
                                 </label>
                             </div>
                             <div className="form-control lg:w-1/3">
                                 <label className="input input-bordered flex items-center gap-2">
                                     Total Visitor
-                                    <input type="text" name="visitor" className="grow w-full" placeholder="Total Visitor Per Year" {...register("TotalVisitor")} />
+                                    <input type="text" required defaultValue={TotalVisitor} name="visitor" className="grow w-full" placeholder="Total Visitor Per Year" {...register("TotalVisitor")} />
                                 </label>
                             </div>
                             <div className="form-control lg:w-1/3">
                                 <label className="input input-bordered flex items-center gap-2">
-                                    Seasonality                         <input type="text" name="description" className="grow w-full" placeholder="Summer/Winter" {...register("Seasonality")} />
+                                    Seasonality                         <input type="text" required defaultValue={Seasonality} name="description" className="grow w-full" placeholder="Summer/Winter" {...register("Seasonality")} />
                                 </label>
                             </div>
                         </div>
                         <div className="form-control ">
                             <label className="input input-bordered flex items-center gap-2">
-                                Description                         <input type="text" name="description" className="grow w-full" placeholder="Description" {...register("Description")} />
+                                Description                         <input type="text" required defaultValue={Description} name="description" className="grow w-full" placeholder="Description" {...register("Description")} />
                             </label>
                         </div>
 
