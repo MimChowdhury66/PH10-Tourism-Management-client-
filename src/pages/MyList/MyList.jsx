7c6f73f98df9cfe2f54d5f83869cda5131ae981a
mim chowdhury <umac3333@gmail.com>
2024-04-30 16:31:49 +0600

    spot-deleted-successfully

--- src/pages/MyList/MyList.jsx
+++ src/pages/MyList/MyList.jsx
@@ -2,17 +2,60 @@ import { useContext, useEffect, useState } from "react";
 import { Zoom } from "react-awesome-reveal";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyList = () => {
     const { user } = useContext(AuthContext);
     const [spots, setSpots] = useState([]);
+    const [deleted, setDeleted] = useState(false);
+
     useEffect(() => {
         fetch(`http://localhost:5000/myList/${user?.email}`)
             .then(res => res.json())
             .then(data => {
                 setSpots(data)
             })
-    }, [user])
+    }, [user, deleted]);
+
+
+    const handleDelete = (id) => {
+        // console.log(id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`http://localhost:5000/delete/${id}`, {
+                        method: 'DELETE',
+
+                    })
+
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data)
+                            if (data.deletedCount > 0) {
+                                Swal.fire(
+                                    {
+                                        icon: "success",
+                                        title: "Tourist spot deleted",
+                                    }
+                                )
+                                setDeleted(!deleted)
+                            }
+                        })
+                }
+            })
+
+    }
+
+
+
 
     return (
         <div>
@@ -35,7 +78,9 @@ const MyList = () => {
                     <tbody>
                         {/* row 1 */}
                         {
-                            spots.map((spot, index) => (
+                            spots.map((spot, index) =>
+
+                            (
                                 <tr>
 
                                     <th>{index + 1}</th>
@@ -45,7 +90,7 @@ const MyList = () => {
                                     <td>{spot.TotalVisitor} </td>
                                     <td className="flex gap-2 m-2">
                                         <Link to={`/updateSpot/${spot._id}`}><button className="btn text-white  bg-orange-400 ">Update</button></Link>
-                                        <button className="btn text-white  bg-black ">Delete</button>
+                                        <button onClick={() => handleDelete(spot._id)} className="btn text-white  bg-black ">Delete</button>
                                     </td>
                                 </tr>
                             ))
